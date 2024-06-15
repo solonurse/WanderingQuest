46bb02c0a2711fbc0339d035d0a15dfdba5eb111
中野駿介 <nakanoshunsuke@nakanoshunsukenoMacBook-Air.local>
2024-06-15 17:08:29 +0900

    useHandleSignInの修正

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -3,12 +3,13 @@
 import React, { useContext } from 'react';
 import { useSession } from 'next-auth/react';
 import Image from "next/image";
-import useHandleSignIn from '@/hooks/useSignIn';
+import useHandleSignIn from '@/hooks/useHandleSignIn';
 import { userContext } from "@/context/UserContext";
 
 const Login = () => {
   const { status } = useSession();
   const user = useContext(userContext);
+  const handleSignIn = useHandleSignIn();
 
   if (status === 'loading') {
 		return <div>Loading...</div>;
@@ -20,13 +21,13 @@ const Login = () => {
         <h1 className="text-4xl text-center mb-6">ユーザーログイン</h1>
         <div className='flex flex-col items-center'>
           <div className="mb-3">
-            <button className='w-56 flex items-center gap-1 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow' onClick={() => useHandleSignIn('google')}>
+            <button className='w-56 flex items-center gap-1 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow' onClick={() => handleSignIn('google')}>
             <Image src="/google_logo.svg" alt="ロゴ画像" width={24} height={24} />
               Google でログイン
             </button>
           </div>
           <div>
-            <button className='w-56 flex items-center gap-1 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow' onClick={() => useHandleSignIn('github')}>
+            <button className='w-56 flex items-center gap-1 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow' onClick={() => handleSignIn('github')}>
             <Image src="/github_logo.svg" alt="ロゴ画像" width={24} height={24} />
               <p>Githubでログイン</p>
             </button>
--- frontend/src/components/PreLoginHeader.tsx
+++ frontend/src/components/PreLoginHeader.tsx
@@ -2,15 +2,16 @@
 
 import Modal from "./Modal";
 import Login from "./Login";
-import useHandleSignIn from '@/hooks/useSignIn';
+import useHandleSignIn from '@/hooks/useHandleSignIn';
 
 const PreLoginHeader = () => {
+  const handleSignIn = useHandleSignIn();
 
   return (
     <div>
       <ul className="flex gap-4 me-3">
         <li>
-          <button className="hover:font-extrabold" onClick={() => useHandleSignIn('credentials')}>
+          <button className="hover:font-extrabold" onClick={() => handleSignIn('credentials')}>
             ゲストログイン
           </button>
         </li>
